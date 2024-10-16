a5f4fbd30655c4d157d042d09eda0bd8c13de538
mivlev <markivlev@gmail.com>
2024-10-15 21:45:40 -0600

    Added player movement and asteroid spawning logic

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
 	pygame.init()
@@ -11,17 +10,27 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	Player.containers = (updatable, drawable)
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-			
-		player.update(dt)
+		
+		for obj in updatable:
+			obj.update(dt)
 		pygame.Surface.fill(screen, color = 'black')
-		player.draw(screen)
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
 
 		# limit framerate to 60 fps
